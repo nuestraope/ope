a12df3166ae2c87c410c2f0664fc813f50162626
vagrant <vagrant@homestead>
2020-12-14 16:26:58 +0000

    commit inicial

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app', ['class' => 'off-canvas-sidebar', 'activePage' => 'home', 'title' => __('Material Dashboard')])
+
+@section('content')
+<div class="container" style="height: auto;">
+  <div class="row justify-content-center">
+      <div class="col-lg-8 col-md-8">
+          <h1 class="text-white text-center">¡Bienvenidos a Nuestra OPE!</h1>
+      </div>
+  </div>
+</div>
+@endsection
