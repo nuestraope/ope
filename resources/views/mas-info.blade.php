4822e1ea755ec6eef30063f983a3b1fc8c458018
Juan Melgar <ranagha@gmail.com>
2021-03-07 20:44:29 +0100

    amigos e info terminados

--- resources/views/mas-info.blade.php
+++ resources/views/mas-info.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container" style="height: auto;position:relative">
   <div class="row justify-content-center">
-      <div class="text-black col-lg-4 col-md-4"><h2>NUESTRA OPE EN NÚMEROS</h2></div>
+      <div class="text-black col-lg-4 col-md-4"><h2>Nuestra Ope en números</h2></div>
       <div class="text-black col-lg-9 col-md-9">
         <ul>
             <li>Más de 3.000 seguidores en YouTube.</li>
@@ -22,6 +22,28 @@
               <li><a href="mailto:nuestraope@gmail.com">Mail: nuestraope@gmail.com</a>
               </li>
           </ul>
+      </div>
+      <div class="text-black col-lg-4 col-md-4"><h2>Academias</h2></div>
+      <div class="text-black col-lg-9 col-md-9">
+          Está muy bien contar con recursos como los vídeos de Nuestra OPE para prepararnos una oposición. Y el que tenga tiempo y ganas, podrá aprender muchísimo elaborando su propio temario. No obstante, para el que pueda permitírselo, yo aconsejo apuntarse a una academia, o como poco comprarse el temario (aunque luego añadas o modifiques cosas a tu criterio). El temario ya elaborado nos facilita y simplifica mucho la tarea, pues hacerse un temario desde cero es duro, complejo y lleva mucho tiempo del que no siempre disponemos, os lo digo por experiencia propia. Y ya si podemos ir a clases mejor que mejor, tendremos más compromiso y rutina con el estudio, además de tener a profesores que nos resuelvan las dudas. Ojo con usar temarios de convocatorias anteriores: se puede estudiar con ellos, siempre es más fácil que partir de cero, pero corremos el riesgo de que haya habido cambios y estudiarlo de forma errónea (cambios en legislación, EPINE, NANDA, DSM… amén de pruebas diagnósticas, procedimientos, farmacología… hay que tener cuidado).
+          Dicho esto, os facilito los enlaces a las diferentes academias que preparan u ofrecen temario para las oposiciones de enfermería / EIR. Comentaros también a los más novatillos, que algunos sindicatos ofrecen clases y libros, así como los colegios de enfermería de algunas CCAA.
+          <ul>
+              <li><a href="https://ifses.es">IFSES</a>
+              </li>
+              <li><a href="https://grupocto.es">CTO</a>
+              </li>
+              <li><a href="https://academiamir.com/cursos-amir/empresas-amigas/amir-enfermeria/">AMIR</a>
+              </li>
+              <li><a href="https://aulaplusformacion.es">AULAPLUS</a>
+              </li>
+              <li><a href="https://on-enfermeria.com">ON ENFERMERIA</a>
+              </li>
+              <li><a href="www.oposalud.es">OPOSALID</a>
+              </li>
+              <li><a href="www.planeir.es">PLAN EIR</a>
+              </li>
+          </ul>
+          Si necesitáis opiniones, información o referencias de forma objetiva, la compañera Ángeles del blog <a href="www.enfermeraconelcorazon.es">“Enfermera con el corazón”</a> hizo un análisis muy completo en <a href="https://www.youtube.com/watch?v=odKtPr5gDR4&t=890s">Youtube</a>.
       </div>
   </div>
 </div>
