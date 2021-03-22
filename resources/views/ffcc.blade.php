fbd33ff35a4328104fa76599ee0ab75c3ec55bd6
Juan Melgar <ranagha@gmail.com>
2021-03-22 20:27:15 +0100

    Formación continuada

--- resources/views/ffcc.blade.php
+++ resources/views/ffcc.blade.php
@@ -3,16 +3,25 @@
 @section('content')
     <div class="container" style="height: auto;position:relative">
         <div class="row justify-content-center">
-            <div class="text-black col-lg-6 col-md-6"><h2>Redes Sociales</h2></div>
+            <div class="text-black col-lg-6 col-md-6"><h2>Formación continuada</h2></div>
             <div class="text-black col-lg-9 col-md-9">
-                Nuestra OPE estaba formado inicialmente por dos personas, somos tres actualmente:
+                En este apartado os voy a facilitar direcciones de algunas academias con las que podéis hacer cursos. Creo que es importante tener en cuenta cinco cosas:
+                <ol>
+                    <li>Una de nuestras obligaciones como personal sanitario es mantener nuestros conocimientos actualizados, de ahí la importancia de hacer cursos y de formarse. Desde mi punto de vista no hay color (en la mayoría de las veces) si comparamos los cursos presenciales con los cursos a distancia. Pero a veces el tiempo, la movilidad o la disponibilidad en general no nos permite elegir otra opción. También he de decir que algunos cursos que he hecho a distancia han estado bastante bien.</li>
+                    <li>Tenemos que hacer 40 horas de formación al año si queremos optar a subir de nivel para la carrera profesional. Todos los años, ininterrumpidamente. Ojalá alguien me lo hubiera dicho en su día, pues hay una época de mi vida que me centré tanto en estudiar para las oposiciones que no hice formación continuada, y eso me impide ahora poder subir de nivel a pesar de haber terminado la carrera en el 2009. Además, en la planilla anual nos suman horas de trabajo si presentamos 40 horas de formación. Lo mires por donde lo mires, nos viene bien.</li>
+                    <li>Academias y cursos hay muchos. Mirad, comparad, elegid lo que os interese. Hay muchas ofertas y promociones que pueden suponer una diferencia económica importante entre dos cursos parecidos. Si lo que buscáis es sumar puntos para el apartado de formación de una oposición (digo oposición porque para las bolsas no suele haber problema), leeros bien las bases a ver qué cursos sirven y cuáles no. Hubo en una oposición en la que no querían contar los cursos de academias tipo Logoss, imaginad la que se lio…
+                    </li>
+                    <li>Hay cursos gratuitos. Con la pandemia del COVID, muchas academias facilitaron formación gratuita al respecto. De hecho, hay academias que ya sin COVID tienen algunos de sus cursos gratis.
+                    </li>
+                    <li>Mirad la página del Colegio de Enfermería que os corresponde. También ofrece cursos: algunos gratuitos, otros pagando pero a un precio más asequible… Si estáis afiliados a algún sindicato, miradlo también. Muchos sindicatos dan X cursos gratis anuales a sus afiliados.
+                    </li>
+                </ol>
+                Los enlaces son: <br/>
                 <ul>
-                    <li><a href="https://www.youtube.com/channel/UC6eK4uuUTeFMWcBYJhZxhpA/videos">Youtube: https://www.youtube.com/channel/UC6eK4uuUTeFMWcBYJhZxhpA/videos</a>
-                    </li>
-                    <li><a href="https://www.facebook.com/NuestraOPE">Facebook: https://www.facebook.com/NuestraOPE</a>
-                    </li>
-                    <li><a href="mailto:nuestraope@gmail.com">Mail: nuestraope@gmail.com</a>
-                    </li>
+                    <li><a href="https://www.formacionalcala.es/comision-de-formacion-continuada?source=google&tag_ref=ads&gclid=Cj0KCQiAgomBBhDXARIsAFNyUqO3Pxs5O1xpsraTGu148rrPX_hznPFmE3eAMitEpPLrlI5JJYsrDX0aAulmEALw_wcB#">Formación Alcalá</a></li>
+                    <li><a href="https://www.fuden.es">Fuden</a></li>
+                    <li><a href="https://www.logoss.net/cursos-line-para-personal-sanitario/enfermeria-cursos">Logoss</a></li>
+                    <li><a href="https://medicarama.com">Medicarama</a></li>
                 </ul>
             </div>
         </div>
