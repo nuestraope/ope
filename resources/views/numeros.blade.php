01f5b527223853f65fbb828813c83716b479f4b7
Juan Melgar <ranagha@gmail.com>
2021-04-14 18:55:54 +0200

    numeros mejorado

--- resources/views/numeros.blade.php
+++ resources/views/numeros.blade.php
@@ -10,10 +10,13 @@
                     <li>Algunos de nuestros vídeos cuentan con más de 10.000 visualizaciones.</li>
                     <li>Tenemos más de 1.500 amigos en Facebook.</li>
                 </ul>
+                <div class="text-center">
+                    <a href="https://www.facebook.com/NuestraOPE"><img src="img/facebook.gif" title="Siguenos en Facebook" width="25%" style="margin:60px"></a>
+                    <a href="https://www.youtube.com/channel/UC6eK4uuUTeFMWcBYJhZxhpA/videos"><img src="img/youtube.gif" title="Siguenos en Youtube" width="25%" style="margin:60px"></a>
+                    <a href="https://twitter.com/NuestraOpe"><img src="img/twitter.gif" title="Siguenos en Twitter" width="25%" style="margin:60px"></a>
+                    <a href="https://tiktok.com/NuestraOpe"><img src="img/tiktok.gif" title="Siguenos en Tiktok" width="25%" style="margin:60px"></a>
+                </div>
             </div>
-        </div>
-        <div class="text-center">
-            <img width="400px" src="img/sosa.jpg"/>
         </div>
     </div>
 @endsection
